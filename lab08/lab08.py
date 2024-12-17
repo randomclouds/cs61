7bb5be96c8247052fb675f061be5d1a66370f896
randomclouds <3385398842@qq.com>
2024-12-18 00:17:16 +0800

    Finish Lab08 And Add Lab 09

--- lab08/lab08.py
+++ lab08/lab08.py
@@ -12,7 +12,13 @@ def cumulative_mul(t):
     Tree(5040, [Tree(60, [Tree(3), Tree(4), Tree(5)]), Tree(42, [Tree(7)])])
     """
     "*** YOUR CODE HERE ***"
-
+    def help(t):  # I like it
+        all = 1
+        for branch in t.branches:
+            all *= help(branch)
+        t.label *= all
+        return t.label
+    help(t)
 
 def prune_small(t, n):
     """Prune the tree mutatively, keeping only the n branches
@@ -31,11 +37,11 @@ def prune_small(t, n):
     >>> t3
     Tree(6, [Tree(1), Tree(3, [Tree(1), Tree(2)])])
     """
-    while ____:
-        largest = max(____, key=____)
+    while len(t.branches) > n:
+        largest = max(t.branches, key=lambda t: t.label)
         t.branches.remove(largest)
     for b in t.branches:
-        ____
+        prune_small(b, n)
 
 
 def delete(t, x):
@@ -58,13 +64,13 @@ def delete(t, x):
     Tree(1, [Tree(4), Tree(5), Tree(3, [Tree(6)]), Tree(6), Tree(7), Tree(8), Tree(4)])
     """
     new_branches = []
-    for _________ in ________________:
-        _______________________
+    for b in t.branches:
+        delete(b, x)
         if b.label == x:
-            __________________________________
+            new_branches.extend(b.branches)
         else:
-            __________________________________
-    t.branches = ___________________
+            new_branches.append(b)
+    t.branches = new_branches
 
 
 def max_path_sum(t):
