676c768e757edf490e8a0c43f9117c21ab4b4de4
randomclouds <3385398842@qq.com>
2024-12-21 20:49:24 +0800

    Finish hw08 and add lab10

--- hw08/hw08.scm
+++ hw08/hw08.scm
@@ -1,7 +1,37 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s) 
+  (cond
+    ((or (null? s) (null? (cdr s))) #t)
+    (else
+      (and
+        (<= (car s) (car (cdr s)))
+        (ascending? (cdr s))
+        ))))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+  (cond
+    ((null? s) '())
+    ((pred (car s))
+      (cons (car s)
+        (my-filter pred (cdr s))))
+    (else
+      (my-filter pred (cdr s)))))
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+  (cond
+    ((null? lst1) lst2)
+    ((null? lst2) lst1)
+    (else
+      (cons
+        (car lst1)
+        (cons
+          (car lst2)
+            (interleave
+              (cdr lst1)
+              (cdr lst2)))))))
+
+(define (no-repeats s)
+  (if (null? s)
+    s
+    (cons (car s)
+      (no-repeats (filter (lambda (x) (not (= x (car s) ))) (cdr s))))))
